beff839055cd0982654b5effb610ca1876dd3c32
Surya Jasper <suryajasper2004@gmail.com>
2020-01-10 23:51:40 -0800

    Set up vscode properly and restructured file order

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -121,10 +121,11 @@ class Robot : public frc::TimedRobot {
   }
 
   void TeleopPeriodic() {
-    // Drive with arcade style
-    //m_robotDrive.ArcadeDrive(-m_stick.GetY(), m_stick.GetX());
+    // testing forward motion
+    moveForward(1);
+    if (isMovingForward()) {
 
-    
+    }
   }
 };
 
